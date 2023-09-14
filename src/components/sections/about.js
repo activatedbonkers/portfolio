2effca0f5b4ef6bd7bead4a6ed7feab995f76c24
Syed Hussaini <syedhussaini@Syeds-MacBook-Pro.local>
2023-09-15 04:11:56 +0530

    updated logos and ico, title

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,9 +135,9 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+              Hello! My name is Syed Junaid Hussaini and I enjoy creating things that live on the
+              internet. My interest in web development started back in 2016 when I decided to try
+              drawing using CSS — turns out hacking together a custom reblog button taught me a lot
               about HTML &amp; CSS!
             </p>
 
